24ee23af13aa19790be7d8c8625b90ae1b5803bf
zezking <zhaoenze001@gmail.com>
2021-03-03 12:08:10 -0800

    add seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+    users (
+        id,
+        student_id,
+        teacher_id,
+        assignment_id,
+        created_at,
+        started_at,
+        completed_at,
+        student_feedback,
+        teacher_feedback
+    )
+VALUES
+    (
+        1,
+        1,
+        1,
+        6,
+        '2018-02-12T08:00:00.000Z',
+        '2018-02-12T08:06:00.000Z',
+        '2018-02-12T08:13:20.000Z',
+        'Good',
+        'Good'
+    );
