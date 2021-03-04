f87da2ed35715d0f876b48c4d8d050a9189b1c50
zezking <zhaoenze001@gmail.com>
2021-03-03 19:45:45 -0800

    finish adduser

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -48,10 +48,15 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // return pool.query('INSERT')
+  console.log(user);
+  return pool
+    .query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3);", [
+      user.name,
+      user.email,
+      user.password,
+    ])
+    .catch((err) => console.log(err));
 };
 exports.addUser = addUser;
 
