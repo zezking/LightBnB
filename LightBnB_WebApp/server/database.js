8f371126fd5901a2e17d972e70739ebd16520190
zezking <zhaoenze001@gmail.com>
2021-03-03 20:11:29 -0800

    finish reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -68,7 +68,12 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      "SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating FROM properties JOIN reservations ON properties.owner_id = reservations.guest_id JOIN property_reviews ON property_reviews.guest_id = properties.owner_id WHERE properties.owner_id = $1 AND reservations.end_date < now() :: date GROUP BY reservations.id, properties.id ORDER BY reservations.start_date LIMIT $2;",
+      [guest_id, limit]
+    )
+    .then((data) => data.rows);
 };
 exports.getAllReservations = getAllReservations;
 
